6b056184efc52e793d862e8cf2ed3b5b34665c98
HosnainRafi <rhosnain@gmail.com>
2021-11-12 06:16:01 +0600

    Done

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -16,7 +16,7 @@ const MakeAdmin = () => {
     const handleAdminSubmit = e =>{
         const user = {email}
 
-        fetch('http://localhost:5000/users/admin', {
+        fetch('https://stormy-wave-51397.herokuapp.com/users/admin', {
             method: 'PUT',
             headers: {
                 'authorization': `Bearer ${token}`,
